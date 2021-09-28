fa1856cd797b333ba206c6ee3ad2aafe7f629d2f
Samuel Queiroz Silva <samuel.silva@itriad.org.br>
2021-09-28 14:35:36 -0400

    First Commit

---
+++ Dominio/Interfaces/Genericos/IGenericos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces.Genericos
+{
+    public interface IGenericos<T> where T : class
+    {
+
+        Task Adicionar(T Obj);
+        Task Atualizar(T Obj);
+        Task Excluir(T Obj);
+        Task<T> BuscarPorId(int Id);
+        Task<List<T>> Listar();
+    }
+}
---
+++ Dominio/Servicos/ServicoNoticias.cs
@@ -0,0 +1,28 @@
+﻿using Dominio.Interfaces.InterfaceServicos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Servicos
+{
+    class ServicoNoticias : IServicoNoticia
+    {
+        public Task AdicionaNoticia(Noticia noticia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizaNoticia(Noticia noticia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Noticia>> listarNoticiaAtiva()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
